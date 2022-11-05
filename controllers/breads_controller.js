b67f79b784c916c6f495e64c9e4f56e98b74f7c0
Jake Casartelli <casartellijake@yahoo.com>
2022-11-05 11:30:05 -0400

    added hook

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,16 +14,21 @@ breads.get('/new', (req, res) => {
       })
 })
 
-// INDEX
+// Index:
 breads.get('/', (req, res) => {
-  Bread.find()
+  Baker.find()
+    .then(foundBakers => {
+      Bread.find()
       .then(foundBreads => {
           res.render('index', {
               breads: foundBreads,
+              bakers: foundBakers,
               title: 'Index Page'
           })
       })
+    })
 })
+
 
 // CREATE
 breads.post('/', (req, res) => {
